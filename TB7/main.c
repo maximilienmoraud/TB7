961c5849ba10179a90e1780185e462ddffcd835d
maximilien moraud <maximilienmoraud@MacBook-Pro-de-Maximilien.local>
2019-03-18 12:46:29 +0100

    Initial Commit

---
+++ TB7/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  TB7
+//
+//  Created by Maximilien Moraud on 18/03/2019.
+//  Copyright © 2019 Maximilien Moraud. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
